ef51bcab4374564d56ae3ff274e55f659cfaa3db
qwartzqq <145793739+qwartzqq@users.noreply.github.com>
2025-05-23 20:19:13 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748020753672

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_0637ee40._.js",
+  "static/chunks/src_c63d87a2._.js"
+],
+    source: "dynamic"
+});
